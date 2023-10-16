ce08114d0c3b77c6fdd5cfcabf91a856e3342cb8
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2023-10-16 09:53:40 -0300

    Form Cadastro

--- src/routes/Inserir.jsx
+++ src/routes/Inserir.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ListaProdutos } from "../components/ListaProdutos";
 import { useNavigate } from "react-router-dom";
 import NavLogin from "../components/NavLogin";
-import '../css/Inserir.css'
+import "../css/Inserir.css";
 
 function Inserir() {
   const navigate = useNavigate();
@@ -34,59 +34,60 @@ function Inserir() {
   return (
     <>
       <NavLogin />
-      <section>
+      <section className="section-inserir">
         <h1>Cadastro de Produtos</h1>
         <form onSubmit={handleSubmit}>
-          <fieldset>
-            <legend>PRODUTO</legend>
-            <p>
-              <label htmlFor="idNome"> Nome do Produto:</label>
-              <input
-                type="text"
-                name="nome"
-                id="idNome"
-                value={produto.nome}
-                onChange={handleChange}
-                className="input_field" required
-              />
-            </p>
-            <p>
-              <label htmlFor="idDesc">Descrição Produto:</label>
-              <input
-                type="text"
-                name="desc"
-                id="idDesc"
-                value={produto.desc}
-                onChange={handleChange}
-                className="input_field" required
-              />
-            </p>
-            <p>
-              <label htmlFor="idValor"> Valor do Produto:</label>
-              <input
-                type="text"
-                name="valor"
-                id="idValor"
-                value={produto.valor}
-                onChange={handleChange}
-                className="input_field" required
-              />
-            </p>
-            <p>
-              <label htmlFor="idImg"> Imagem do Produto:</label>
-              <input
-                type="text"
-                name="img"
-                id="idImg"
-                value={produto.img}
-                onChange={handleChange}
-                className="input_field" required
-              />
-            </p>
-            <p>
-              <button type="submit">CADASTRAR</button>
-            </p>
-          </fieldset>
+          <p>
+            <label htmlFor="idNome"> Nome do Produto:</label>
+            <input
+              type="text"
+              name="nome"
+              id="idNome"
+              value={produto.nome}
+              onChange={handleChange}
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor="idDesc">Descrição Produto:</label>
+            <input
+              type="text"
+              name="desc"
+              id="idDesc"
+              value={produto.desc}
+              onChange={handleChange}
+              className="input_field"
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor="idValor"> Valor do Produto:</label>
+            <input
+              type="text"
+              name="valor"
+              id="idValor"
+              value={produto.valor}
+              onChange={handleChange}
+              className="input_field"
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor="idImg"> Imagem do Produto:</label>
+            <input
+              type="text"
+              name="img"
+              id="idImg"
+              value={produto.img}
+              onChange={handleChange}
+              className="input_field"
+              placeholder="URL"
+              required
+            />
+          </p>
+          <p>
+            <button type="submit">CADASTRAR</button>
+          </p>
         </form>
       </section>
     </>
