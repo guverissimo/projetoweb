060bdefe092fd575b104022de5d2213c41ab8f78
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2023-10-16 10:01:00 -0300

    footer att

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,24 +1,44 @@
 import {} from "react";
 import "../css/Footer.css";
-import { AiFillFacebook,AiFillInstagram,AiFillLinkedin,AiFillMail } from "react-icons/ai";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+} from "react-icons/ai";
 function Ropade() {
   return (
     <>
       <footer>
         <p className="p_rede">Redes Sociais</p>
         <div className="redes-sociais">
-          <a className="icon" href="https://www.facebook.com/fiap?ref=embed_page" target="blank">
-          <AiFillFacebook></AiFillFacebook>
+          <a
+            className="icon"
+            href="https://www.facebook.com/fiap?ref=embed_page"
+            target="blank"
+          >
+            <AiFillFacebook></AiFillFacebook>
           </a>
           <a className="icon" href="https://www.instagram.com/">
             <AiFillInstagram></AiFillInstagram>
           </a>
-          <a className="icon" href="#https://br.linkedin.com/?original_referer=https%3A%2F%2Fwww.google.com%2F">
+          <a
+            className="icon"
+            href="#https://br.linkedin.com/?original_referer=https%3A%2F%2Fwww.google.com%2F"
+          >
             <AiFillLinkedin></AiFillLinkedin>
           </a>
           <a className="icon" href="mailto:fiap@fiap.com">
             <AiFillMail></AiFillMail>
           </a>
+        </div>
+
+        <div className="get-email">
+          <div>
+            <h2>Fique por dentro de Promoções</h2>
+            <p>Cadastre seu Email para receber cupons de desconto!</p>
+          </div>
+          <input type="email" placeholder="Digite seu melhor Email" />
         </div>
       </footer>
     </>
